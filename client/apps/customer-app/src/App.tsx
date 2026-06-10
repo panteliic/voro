9118b81ee7e9ec37358da3e48451c772944233d2
Nikola Pantelic <pantelicnikolaa@gmail.com>
2026-06-10 18:47:26 +0200

    Update client app

--- client/apps/customer-app/src/App.tsx
+++ client/apps/customer-app/src/App.tsx
@@ -1,11 +1,12 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
 
 function App() {
-  
   return (
-
-    <div>
-      
-    </div>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
   )
 }
 
